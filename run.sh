b4bf09a5272373a870802fd8ba079c3c0463d76c
Adam Retter <adam.retter@googlemail.com>
2014-04-23 10:49:37 +0100

    Preserve existing MAVEN_OPTS

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
-export MAVEN_OPTS="-Djava.library.path=/lib64:/usr/local/lib/jni -XX:MaxPermSize=512m -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=4000"
+export MAVEN_OPTS="$MAVEN_OPTS -Djava.library.path=/lib64:/usr/local/lib/jni -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=4000"
+export MAVEN_OPTS="$MAVEN_OPTS -XX:MaxPermSize=512m"
 export JAVA_HOME="/usr/lib/jvm/java-1.7.0-openjdk.x86_64"
 
 mvn jetty:run -Djetty.port=8084
