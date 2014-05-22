57d1c72d4eebb60e34d9df36edd8509902e36fe6
rwalpole <robkwalpole@gmail.com>
2014-05-22 11:35:28 +0100

    [feature] added new LoaderCatalogueJmsClient and spec test. Modified pom to use dri-catalogue-jms-client

--- src/main/scala/uk/gov/tna/dri/preingest/loader/catalogue/LoaderCatalogueJmsClient.scala
+++ src/main/scala/uk/gov/tna/dri/preingest/loader/catalogue/LoaderCatalogueJmsClient.scala
@@ -1,11 +1,30 @@
 package uk.gov.tna.dri.preingest.loader.catalogue
 
+import uk.gov.tna.dri.catalogue.jms.client.{JmsConfig, CatalogueJmsClient}
+import javax.xml.bind.JAXBElement
+import uk.gov.nationalarchives.dri.ingest.DriUnitsType
+
 /**
  * Created with IntelliJ IDEA.
  * User: Rob Walpole
  * Date: 5/22/14
  * Time: 10:09 AM
  */
-class LoaderCatalogueJmsClient {
+class LoaderCatalogueJmsClient(jmsConfig: JmsConfig) extends CatalogueJmsClient(jmsConfig) {
+
+  def getUnitsLoaded(limit: Int): Option[JAXBElement[DriUnitsType]] = {
+    logger.info("Retrieving " + limit + " loaded units.")
+    val xml = populateXml("getUnitsLoaded", getQuery(10), limitParam)
+    val reply = exchangeMessages(xml, jmsConfig)
+    reply match {
+      case Right(x) => {
+        Some(x.getAny.asInstanceOf[JAXBElement[DriUnitsType]])
+      }
+      case Left(x) => {
+        logger.error("Retrieving loaded units from catalogue returned: " + x)
+        None
+      }
+    }
+  }
 
 }
--- src/test/scala/uk/gov/tna/dri/preingest/loader/catalogue/LoaderCatalogueJmsClientSpec.scala
+++ src/test/scala/uk/gov/tna/dri/preingest/loader/catalogue/LoaderCatalogueJmsClientSpec.scala
@@ -1,11 +1,30 @@
 package uk.gov.tna.dri.preingest.loader.catalogue
 
+import org.specs2.mutable.Specification
+import uk.gov.nationalarchives.dri.ingest.PartIdType
+import org.specs2.mock.Mockito
+import uk.gov.tna.dri.catalogue.jms.client.JmsConfig
+
 /**
  * Created with IntelliJ IDEA.
  * User: Rob Walpole
- * Date: 5/22/14
- * Time: 11:06 AM
+ * Date: 5/21/14
+ * Time: 10:44 AM
  */
-class LoaderCatalogueJmsClientSpec {
+class LoaderCatalogueJmsClientSpec extends Specification with Mockito {
+  //args(skipAll = true)
+  val partId = new PartIdType
+  //val mockJmsConfig = mock[JmsConfig]
+  val jmsConfig = new JmsConfig ("tcp://localhost:61616", "admin", "admin" , "dri.catalogue" , 60000)
+  val catalogueJmsClient = new LoaderCatalogueJmsClient(jmsConfig)
+
+  "The getUnitsLoaded method" should {
+    "return 2 elements" in {
+      catalogueJmsClient.getUnitsLoaded(10).get.getValue.getUnits must have size(2)
+    }
+  }
+
+
+
 
 }
