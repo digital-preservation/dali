6261de3e1314e63e332970000d7a6e2dfdc78615
rwalpole <robkwalpole@gmail.com>
2014-05-22 11:34:45 +0100

    [feature] added new LoaderCatalogueJmsClient and spec test. Modified pom to use dri-catalogue-jms-client

---
+++ src/main/scala/uk/gov/tna/dri/preingest/loader/catalogue/LoaderCatalogueJmsClient.scala
@@ -0,0 +1,11 @@
+package uk.gov.tna.dri.preingest.loader.catalogue
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Rob Walpole
+ * Date: 5/22/14
+ * Time: 10:09 AM
+ */
+class LoaderCatalogueJmsClient {
+
+}
---
+++ src/test/scala/uk/gov/tna/dri/preingest/loader/catalogue/LoaderCatalogueJmsClientSpec.scala
@@ -0,0 +1,11 @@
+package uk.gov.tna.dri.preingest.loader.catalogue
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Rob Walpole
+ * Date: 5/22/14
+ * Time: 11:06 AM
+ */
+class LoaderCatalogueJmsClientSpec {
+
+}
