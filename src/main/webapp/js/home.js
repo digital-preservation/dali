0eec40002d8b7b74109ff15b7dbe851ddf4cb68f
Laura Damian <laura.damian@nationalarchives.gov.uk>
2014-06-09 17:14:45 +0100

    [bugfix] change back the json to reflect last change in ClientAction

--- src/main/webapp/js/home.js
+++ src/main/webapp/js/home.js
@@ -170,7 +170,7 @@ function doStartLoad(pendingUnit, cert, pass) {
   subSocket.push(JSON.stringify({
     actions: [{
         action: 'load',
-        loadUnit: {
+        unit: {
             uid: pendingUnit.uid,
             parts: partsCpy
         },
