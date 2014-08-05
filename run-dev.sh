b2f0226641259795e4c0806f6b19ae8903090d43
Graham Seaman <graham.seaman@nationalarchives.gov.uk>
2014-08-05 17:12:32 +0100

    Use separate configuration files for live/test and dev, set from system property (via new run-dev.sh)

--- run-dev.sh
+++ run-dev.sh
@@ -3,4 +3,4 @@ export MAVEN_OPTS="$MAVEN_OPTS -Djava.library.path=/lib64:/usr/local/lib/jni"
 export MAVEN_OPTS="$MAVEN_OPTS -XX:MaxPermSize=512m"
 export JAVA_HOME="/usr/lib/jvm/java-1.7.0-openjdk.x86_64"
 
-mvn jetty:run -Djetty.port=8084 -Dconfig.resource=/application.conf
+mvn jetty:run -Djetty.port=8084 -Dconfig.resource=/development.conf
