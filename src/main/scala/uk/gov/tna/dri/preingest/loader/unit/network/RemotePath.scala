fa62509b2ddf4d8da5cb497b46b33345c9501509
Graham Seaman <graham.seaman@nationalarchives.gov.uk>
2014-08-29 15:07:00 +0100

    Removed uniqueName, replace with filename

--- src/main/scala/uk/gov/tna/dri/preingest/loader/unit/network/RemotePath.scala
+++ src/main/scala/uk/gov/tna/dri/preingest/loader/unit/network/RemotePath.scala
@@ -2,13 +2,12 @@ package uk.gov.tna.dri.preingest.loader.unit.network
 
 /**
  * Replacement for Path, which works only on local file systems for remote hosts
- * @param uniqueName concatenation of username + timestamp _ filename
  * @param userName   owner of file
- * @param fileName   name of file minus extension
+ * @param fileName   name of file minus extensions
  * @param fileSize   size of file in bytes
  * @param lastModified  timestamp of file
  * @param path       path to file starting from search root
  */
 //class RemotePath(val name:String, val size: Long, val lastModified: Long ) {}
 
-class RemotePath(val uniqueName: String, val userName: String, val fileName: String, val fileSize: Long, val lastModified: Long, val path: String) {}
+class RemotePath(val userName: String, val fileName: String, val fileSize: Long, val lastModified: Long, val path: String) {}
