8fe15e64a63f29e31620164da268bb3231ff065f
Graham Seaman <graham.seaman@nationalarchives.gov.uk>
2015-04-27 14:44:46 +0100

    mvn license:format run on files

--- run-dev.sh
+++ run-dev.sh
@@ -1,3 +1,12 @@
+#
+# Copyright (c) 2013, The National Archives <digitalpreservation@nationalarchives.gov.uk>
+# http://www.nationalarchives.gov.uk
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
 export MAVEN_OPTS="$MAVEN_OPTS -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=9000"
 export MAVEN_OPTS="$MAVEN_OPTS -Djava.library.path=/lib64:/usr/local/lib/jni"
 export MAVEN_OPTS="$MAVEN_OPTS -XX:MaxPermSize=512m"
